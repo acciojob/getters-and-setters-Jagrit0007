161e0cc7a852fb55bf6375ada72897b63454178c
Jagrit Krishna <87370572+Jagrit0007@users.noreply.github.com>
2024-05-13 19:42:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,26 +2,26 @@
 class Person {
 	  constructor(name,age)
 	{
-		this._name=name;
-		this._age=age;
+		this.name=name;
+		this.age=age;
 	}
 	get name(){
 		return this._name;
 	}
     set age(){
-		 this._age=age;
+		 this.age=age;
 	}
 }
 
 class Student extends Person {
 	study(){
-		console.log(`${this._name} is studying`)
+		console.log(`${this.name} is studying`)
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${this._name}` is teaching)
+		console.log(`${this.name}` is teaching)
 	}
 }
 
